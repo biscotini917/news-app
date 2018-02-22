7e0eec84ab94a5dab9a0d2815dc1fbc81a6aeb70
Bruce Leong <brucewleong@gmail.com>
2018-02-22 16:05:20 -0500

    updated

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import database from './index';
+// import { POINT_CONVERSION_COMPRESSED } from 'constants';
 
 export default class Sidebar extends Component {
   constructor() {
     super();
     this.state = {
-      channel: ''
+      channel: '',
+      channels: ''
     }
   this.handleChange = this.handleChange.bind(this);
   this.handleSubmit = this.handleSubmit.bind(this);
+  this.gotData = this.gotData.bind(this);
+  this.errData = this.errData.bind(this);
+
   }
 
   handleChange(evt) {
@@ -17,12 +22,23 @@ export default class Sidebar extends Component {
 
   handleSubmit(evt) {
     evt.preventDefault();
-    database.ref('Channels').push({name: 'bruce', channelName: this.state.channel})
-    this.setState({channel: ''})
+    database.ref('Channels').push({name: 'bruce', channelName: this.state.channel});
+    this.setState({channel: '', newChannels: '', name: ''});
+    database.ref('Channels').on('value', this.gotData, this.errData);
+  }
+
+  gotData(data) {
+    let channels = data.val()
+    this.setState({channels})
+  }
+
+  errData(err) {
+    console.log('Error!');
+    console.log(err);
   }
 
   render() {
-    console.log('state when typing in sidebar', this.state)
+    console.log(this.state)
     return (
       <sidebar>
       <form onSubmit={this.handleSubmit}>
@@ -45,8 +61,29 @@ export default class Sidebar extends Component {
         <section>
           <h4 className="menu-item">Your Channels
         </h4>
+        {
+          (for var key in this.state.channels) {
+
+          }
+        }
         </section>
-      </sidebar>
-    );
+        </sidebar>
+      );
+    }
   }
-}
+  // {
+  //   (this.state.channels === 0)
+  //   ? <h2> There are no channels </h2>
+  //   : this.state.channels
+  // }
+
+// gotData(data) {
+//   let channels = data.val()
+//   let keys = Object.keys(channels);
+//   for (let i = 0; i < keys.length; i++) {
+//     let k = keys[i]
+//     let channelName = channels[k].channelName;
+//     let name = channels[k].name;
+//     console.log(name, channelName)
+//   }
+// }
